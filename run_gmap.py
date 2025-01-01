1fcdd8b94c9e5c0e5c28a4cfb8a0d62e8780f9bd
Thanh Hoang <thanhhl95@gmail.com>
2025-01-01 10:21:05 +0700

    misc

--- run_gmap.py
+++ run_gmap.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     # Constants
     schema = 'gmap'
     df_path='raw\location-history-20241218.json'
-    from_date = 20241114
+    from_date = 20241202
     to_date = from_date
 
     # Convert dates to UNIX timestamps
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     # export_dim_location()
     visit, activity, timelinepath = pre_process_json(df_path)
     export_timelinepath(timelinepath, schema=schema, from_date_unix=from_date_unix, to_date_unix=to_date_unix)
-    export_visit(visit, schema=schema, from_date_unix=from_date_unix, to_date_unix=to_date_unix)
-    export_activity(activity, schema=schema, from_date_unix=from_date_unix, to_date_unix=to_date_unix)
+    # export_visit(visit, schema=schema, from_date_unix=from_date_unix, to_date_unix=to_date_unix)
+    # export_activity(activity, schema=schema, from_date_unix=from_date_unix, to_date_unix=to_date_unix)
