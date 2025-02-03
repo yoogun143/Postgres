eab9b68deda9a3c0ef96bfd44ff336f9b338a6b5
Thanh Hoang <Thanh Hoang>
2025-02-03 22:53:06 +0700

    add docstring and annotation

--- run_screentime.py
+++ run_screentime.py
@@ -15,7 +15,17 @@ knowledge_db = os.path.expanduser("~/Library/Application Support/Knowledge/knowl
 schema = 'apple'
 table = 'fact_screentime'
 
-def get_last_created_at(schema='apple', table='fact_screentime'):
+def get_last_created_at(schema: str = 'apple', table: str = 'fact_screentime') -> float:
+    """
+    Retrieves the last created_at value from the specified table in the specified schema, or 0 if no records exist.
+
+    Args:
+        schema (str): The name of the Postgres schema containing the table. Defaults to 'apple'.
+        table (str): The name of the table containing the created_at column. Defaults to 'fact_screentime'.
+
+    Returns:
+        float: The last created_at value from the specified table, or 0 if no records exist.
+    """
     try:
         config = load_config()
         with psycopg2.connect(**config) as conn:
@@ -35,99 +45,88 @@ def get_last_created_at(schema='apple', table='fact_screentime'):
 
     return last_created_at
 
-def sqlite_to_dataframe(last_created_at):
-    # Check if knowledgeC.db exists
-    if not os.path.exists(knowledge_db):
-        print("Could not find knowledgeC.db at %s." % (knowledge_db))
-        exit(1)
+def sqlite_to_dataframe(last_created_at: float) -> pd.DataFrame:
+    """
+    Reads data from the SQLite database located at `knowledge_db` and returns it as a Pandas DataFrame.
 
-    # Check if knowledgeC.db is readable
-    if not os.access(knowledge_db, os.R_OK):
-        print("The knowledgeC.db at %s is not readable.\nPlease grant full disk access to the application running the script (e.g. Terminal, iTerm, VSCode etc.)." % (knowledge_db))
-        exit(1)
+    Parameters
+    ----------
+    last_created_at : float
+        The last created_at value to filter records from the database.
 
-    # Connect to the SQLite database
-    with sqlite3.connect(knowledge_db) as con:
-        cur = con.cursor()
-
-        # Execute the SQL query to fetch data
-        # Modified from https://rud.is/b/2019/10/28/spelunking-macos-screentime-app-usage-with-r/
-        query = """
-        SELECT
-            ZOBJECT.ZVALUESTRING AS "app",
-            (ZOBJECT.ZENDDATE - ZOBJECT.ZSTARTDATE) AS "usage_time",
-            (ZOBJECT.ZSTARTDATE + 978307200) as "start_time",
-            (ZOBJECT.ZENDDATE + 978307200) as "end_time",
-            (ZOBJECT.ZCREATIONDATE + 978307200) as "created_at",
-            ZOBJECT.ZSECONDSFROMGMT AS "tz",
-            ZSOURCE.ZDEVICEID AS "device_id",
-            ZMODEL AS "device_model"
-        FROM
-            ZOBJECT
-            LEFT JOIN
-            ZSTRUCTUREDMETADATA
-            ON ZOBJECT.ZSTRUCTUREDMETADATA = ZSTRUCTUREDMETADATA.Z_PK
-            LEFT JOIN
-            ZSOURCE
-            ON ZOBJECT.ZSOURCE = ZSOURCE.Z_PK
-            LEFT JOIN
-            ZSYNCPEER
-            ON ZSOURCE.ZDEVICEID = ZSYNCPEER.ZDEVICEID
-        WHERE
-            ZSTREAMNAME = "/app/usage" AND
-            (ZOBJECT.ZCREATIONDATE + 978307200) > ?
-        ORDER BY
-            ZCREATIONDATE DESC
-        """
-        cur.execute(query, (last_created_at,))
-
-        # Fetch all rows from the result set
-        df = pd.DataFrame(cur.fetchall())
-
-        if len(df) == 0:
-            raise ValueError(f"No new data found after {last_created_at}")
-        
-        df.columns = [desc[0] for desc in cur.description]
-
-    return df
+    Returns
+    -------
+    df : pandas.DataFrame
+        A Pandas DataFrame containing the data fetched from the database.
+    """
+    ...
 
 def dataframe_to_postgres(df:pd.DataFrame, schema:str, table:str) -> None:
-        df.columns = [x.lower() for x in df.columns] #postgres not like uppercase column name
+    """
+    Inserts a Pandas DataFrame into a PostgreSQL table.
 
-        commands = sql_to_list(f'etl/sql/{schema}/{table}/create_table.sql')
-        warehouse_columns = get_column_name_from_create_table(commands[0])
-        
-        # Sort Pandas columns based on CREATE TABLE sql statement
-        df = df[warehouse_columns]
-        df = df.fillna(AsIs('Null'))
+    This function processes a DataFrame by adjusting its column names to lowercase,
+    ordering the columns based on an SQL CREATE TABLE statement, filling null values,
+    and removing duplicates. The data is then inserted into a specified PostgreSQL table
+    using batch execution.
 
-        # Remove dulicates
-        df = df.drop_duplicates()
+    Parameters:
+    ----------
+    df : pd.DataFrame
+        The DataFrame containing the data to be inserted.
+    schema : str
+        The PostgreSQL schema name where the table resides.
+    table : str
+        The PostgreSQL table name to which the data will be inserted.
 
-        df_columns = list(df)
-        # create (col1,col2,...)
-        columns = ','.join(list(df))
+    Returns:
+    -------
+    None
 
-        # create VALUES('%s', '%s",...) one '%s' per column
-        values = "VALUES({})".format(",".join(["%s" for _ in df_columns])) 
+    Raises:
+    ------
+    psycopg2.DatabaseError
+        If there is a PostgreSQL error during the execution.
+    Exception
+        For any other error that occurs.
+    """
 
-        #create INSERT INTO table (columns) VALUES('%s',...)
-        insert_stmt = f"INSERT INTO {schema}.{table} ({columns}) {values}"
+    df.columns = [x.lower() for x in df.columns] #postgres not like uppercase column name
 
-        try:
-            config = load_config()
-            with psycopg2.connect(**config) as conn:
-                with conn.cursor() as cur:
+    commands = sql_to_list(f'etl/sql/{schema}/{table}/create_table.sql')
+    warehouse_columns = get_column_name_from_create_table(commands[0])
+    
+    # Sort Pandas columns based on CREATE TABLE sql statement
+    df = df[warehouse_columns]
+    df = df.fillna(AsIs('Null'))
 
-                    ### Method 1:copy from pandas
-                    extras.execute_batch(cur, insert_stmt, df.values)
+    # Remove dulicates
+    df = df.drop_duplicates()
 
-                    conn.commit()
+    df_columns = list(df)
+    # create (col1,col2,...)
+    columns = ','.join(list(df))
 
-                    print(f"Inserted {len(df)} rows from sqlite to warehouse: {table}")
+    # create VALUES('%s', '%s",...) one '%s' per column
+    values = "VALUES({})".format(",".join(["%s" for _ in df_columns])) 
 
-        except (psycopg2.DatabaseError, Exception) as error:
-            print(error)
+    #create INSERT INTO table (columns) VALUES('%s',...)
+    insert_stmt = f"INSERT INTO {schema}.{table} ({columns}) {values}"
+
+    try:
+        config = load_config()
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+
+                ### Method 1:copy from pandas
+                extras.execute_batch(cur, insert_stmt, df.values)
+
+                conn.commit()
+
+                print(f"Inserted {len(df)} rows from sqlite to warehouse: {table}")
+
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
 
 if __name__ == '__main__':
     create_table(schema=schema,table=table)
