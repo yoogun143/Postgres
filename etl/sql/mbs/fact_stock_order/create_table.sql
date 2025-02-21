cab5bdc382433718daf934ee42e537fb60ecc75b
Thanh Hoang <thanhhl95@gmail.com>
2025-02-21 21:18:12 +0700

    wip 2

---
+++ etl/sql/mbs/fact_stock_order/create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS %(schema)s.%(table)s
+  (
+    date               DATE
+    ,account           VARCHAR(50)
+    ,symbol            VARCHAR(50)  
+    ,side              VARCHAR(50)
+    ,order_quantity    INT
+    ,order_price       DECIMAL(18,2)
+    ,status            VARCHAR(50)
+    ,order_number      VARCHAR(50)
+    ,channel           VARCHAR(50)
+    ,match_quantity    INT
+    ,match_price       DECIMAL(18,2)
+    ,match_time        TIMESTAMP
+  ); 
--- mbs.py
+++ mbs.py
@@ -1,11 +1,13 @@
+import os
 import pandas as pd
+from load_warehouse import pandas_to_warehouse
 
 # Load the Excel file
 file_path = r"D:\OneDrive - Northeastern University\MBS\Data\order history.xlsx"
-fact_stock_order = pd.read_excel(file_path, sheet_name="Sheet1")
+df = pd.read_excel(file_path, sheet_name="Sheet1")
 
 # Rename columns
-fact_stock_order.rename(columns={
+df.rename(columns={
     "NGÀY ĐẶT LỆNH": "Date",
     "TÀI KHOẢN": "Account",
     "MÃ CK": "Symbol",
@@ -21,83 +23,34 @@ fact_stock_order.rename(columns={
 }, inplace=True)
 
 # Convert data types
-fact_stock_order["Date"] = pd.to_datetime(fact_stock_order["Date"], dayfirst=True, errors="coerce")
-fact_stock_order["Order_quantity"] = pd.to_numeric(fact_stock_order["Order_quantity"], errors="coerce")
-fact_stock_order["Order_price"] = pd.to_numeric(fact_stock_order["Order_price"], errors="coerce")
-fact_stock_order["Match_quantity"] = pd.to_numeric(fact_stock_order["Match_quantity"], errors="coerce")
-fact_stock_order["Match_price"] = pd.to_numeric(fact_stock_order["Match_price"], errors="coerce")
-fact_stock_order["Match_time"] = pd.to_datetime(fact_stock_order["Match_time"], format="%H:%M:%S", errors="coerce").dt.time
+df["Date"] = pd.to_datetime(df["Date"], dayfirst=True, errors="coerce")
+df["Order_quantity"] = pd.to_numeric(df["Order_quantity"], errors="coerce")
+df["Order_price"] = pd.to_numeric(df["Order_price"], errors="coerce")
+df["Match_quantity"] = pd.to_numeric(df["Match_quantity"], errors="coerce")
+df["Match_price"] = pd.to_numeric(df["Match_price"], errors="coerce")
+df["Match_time"] = pd.to_datetime(df["Match_time"], format="%H:%M:%S", errors="coerce").dt.time
 
 # Convert text to uppercase
-fact_stock_order["Side"] = fact_stock_order["Side"].str.upper()
-fact_stock_order["Status"] = fact_stock_order["Status"].str.upper()
+df["Side"] = df["Side"].str.upper()
+df["Status"] = df["Status"].str.upper()
 
 # Pad Account column with leading zeros
-fact_stock_order["Account"] = fact_stock_order["Account"].astype(str).str.zfill(7)
+df["Account"] = df["Account"].astype(str).str.zfill(7)
 
 # Reorder columns
 columns_order = ["Date", "Account", "Symbol", "Side", "Order_quantity", "Order_price",
                  "Status", "Order_number", "Channel", "Match_quantity", "Match_price", "Match_time"]
-fact_stock_order = fact_stock_order[columns_order]
+df = df[columns_order]
+df
+
+# Store data in dim_location in PostgreSQL
+schema = 'mbs'
+table = 'fact_stock_order'
+pandas_to_warehouse(df, schema=schema, table=table)
 
 ######################################################################################################################
 
-import pandas as pd
-import math
-
-# Filter rows where Status is "ĐÃ KHỚP"
-filtered_df = fact_stock_order[fact_stock_order['Status'] == 'ĐÃ KHỚP']
-
-# Add Match_value column
-filtered_df['Match_value'] = filtered_df['Match_price'] * filtered_df['Match_quantity'] * 1000
-
-# Ensure Match_value is treated as numeric
-filtered_df['Match_value'] = pd.to_numeric(filtered_df['Match_value'], errors='coerce')
-
-# Append trading_fee DataFrame
-appended_df = pd.concat([filtered_df, trading_fee], ignore_index=True)
-
-# Sort by Date
-sorted_df = appended_df.sort_values(by='Date')
-
-# Fill down the fee_rate column
-sorted_df['fee_rate'] = sorted_df['fee_rate'].ffill()
-
-# Filter rows where 'flag' is null
-fact_cash_statement_gen = sorted_df[sorted_df['flag'].isnull()]
-
-# Remove unwanted columns
-fact_cash_statement_gen = fact_cash_statement_gen.drop(columns=['end_date', 'valid', 'flag'])
-
-# Add Trading_fee column
-fact_cash_statement_gen['Trading_fee'] = (fact_cash_statement_gen['Match_value'] * fact_cash_statement_gen['fee_rate'] / 100).apply(math.ceil)
-
-# Remove more columns
-fact_cash_statement_gen = fact_cash_statement_gen.drop(columns=[
-    'Order_quantity', 'Order_price', 'Status', 'Match_quantity', 
-    'Match_price', 'Match_time', 'fee_rate', 'Channel', 'Symbol'
-])
-
-# Rename Order_number to Description
-fact_cash_statement_gen.rename(columns={'Order_number': 'Description'}, inplace=True)
-
-# Add prefix to Description column
-fact_cash_statement_gen['Description'] = 'order_number: ' + fact_cash_statement_gen['Description'].astype(str)
-
-# Final result
-fact_cash_statement_gen.reset_index(drop=True, inplace=True)
-
-# Display the resulting DataFrame
-fact_cash_statement_gen.head()
-
-######################################################################################################################
-
-import os
-import pandas as pd
-
-folder_path = r"D:\OneDrive - Northeastern University\MBS\Data\sao_ke_tien"
-
-def transform_file(file_path):
+def transform_excel_statemment(file_path):
     # Load Excel file and select 'My Sheet'
     xls = pd.ExcelFile(file_path)
     df = pd.read_excel(xls, sheet_name='My Sheet', skiprows=3)
@@ -168,12 +121,13 @@ def get_category(description):
     else:
         return None
 
-all_data = []
+cash_statement_folder_path = r"D:\OneDrive - Northeastern University\MBS\Data\sao_ke_tien"
+fact_cash_statement_list = []
 
-for file_name in os.listdir(folder_path):
+for file_name in os.listdir(cash_statement_folder_path):
     if file_name.endswith(('.xlsx', '.xls')):
-        file_path = os.path.join(folder_path, file_name)
-        df = transform_file(file_path)
+        file_path = os.path.join(cash_statement_folder_path, file_name)
+        df = transform_excel_statemment(file_path)
 
         # Rename and clean columns
         df.rename(columns={
@@ -188,6 +142,7 @@ for file_name in os.listdir(folder_path):
         # Filter and transform
         df = df[df['Account'].notnull()]
         df['Account'] = df['Account'].apply(lambda x: str(x).zfill(7))
+        df['Account'] = df['Account'].str.replace('\'','')
         df['Txtype'] = df.apply(lambda row: 'Debit' if row['Credit'] == 0 else 'Credit', axis=1)
         df['Amount'] = df[['Debit', 'Credit']].max(axis=1)
         df['Category'] = df['Description'].apply(get_category)
@@ -199,7 +154,37 @@ for file_name in os.listdir(folder_path):
         exclude_categories = ['Buy Fee', 'Depository Trading Fee', 'Income Tax', 'Margin Recover', 'Margin Release', 'Sale Fee', 'Stock Buy', 'Stock Sale']
         df = df[~df['Category'].isin(exclude_categories)]
 
-        all_data.append(df)
+        fact_cash_statement_list.append(df)
 
-final_df = pd.concat(all_data, ignore_index=True)
-final_df
+fact_cash_statement = pd.concat(fact_cash_statement_list, ignore_index=True)
+fact_cash_statement
+
+######################################################################################################################
+stock_statement_folder_path = r"D:\OneDrive - Northeastern University\MBS\Data\sao_ke_chung_khoan"
+fact_stock_statement_list = []
+
+for file_name in os.listdir(stock_statement_folder_path):
+    if file_name.endswith(('.xlsx', '.xls')):
+        file_path = os.path.join(stock_statement_folder_path, file_name)
+        df = transform_excel_statemment(file_path)
+
+        # Rename and clean columns
+        df.rename(columns={
+            'TÀI KHOẢN': 'Account',
+            'NGÀY THỰC HIỆN': 'Date',
+            'MÃ CHỨNG KHOÁN': 'Symbol',
+            'TRẠNG THÁI': 'Status',
+            'PHÁT SINH TĂNG': 'Credit',
+            'PHÁT SINH GIẢM': 'Debit',
+            'NỘI DUNG': 'Description'
+        }, inplace=True)
+
+        # Filter and transform
+        df = df[df['Account'].notnull()]
+        df['Account'] = df['Account'].apply(lambda x: str(x).zfill(7))
+        df['Account'] = df['Account'].str.replace('\'','')
+
+        fact_stock_statement_list.append(df)
+
+fact_stock_statement = pd.concat(fact_stock_statement_list, ignore_index=True)
+fact_stock_statement
