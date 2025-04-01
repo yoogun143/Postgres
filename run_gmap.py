17bbf2fa82f4d34c93880ff276945bbac182759b
Thanh Hoang <Thanh Hoang>
2025-04-01 21:28:41 +0700

    change docker to add gmap streamlit

--- run_gmap.py
+++ run_gmap.py
@@ -9,9 +9,9 @@ warnings.simplefilter(action='ignore', category=(SettingWithCopyWarning))
 if __name__ == '__main__':
     # Constants
     schema = 'gmap'
-    df_path = 'raw/location-history-20250329.json'
-    from_date = 20250305
-    to_date = 20250329
+    df_path = 'raw/location-history-20250331.json'
+    from_date = 20250330
+    to_date = 20250331
 
     # Convert dates to UNIX timestamps
     from_date_unix = int(datetime.strptime(str(from_date) + ' 00:00:00', '%Y%m%d %H:%M:%S').timestamp()) + 25200 # Add 7 hours
